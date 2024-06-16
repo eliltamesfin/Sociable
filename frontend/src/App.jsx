761e2f1d08ea012b12893980ef1bb4d0d9306d76
Elilta Mesfin Raya <elumesfinraya@gmail.com>
2024-06-16 21:58:46 +0200

    Use router component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import './App.css'
-import Home from "./components/home/Home"
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Home from "./components/home/Home";
 import Layout from "./components/layout/Layout";
 import User from "./components/user/User";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="user/:id" element={<User />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="user/:id" element={<User />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
